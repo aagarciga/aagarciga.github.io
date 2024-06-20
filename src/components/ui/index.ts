d86da277fb9c96acff7c39f9984eddf4fcda49be
Alex Alvarez Gárciga <aagarciga@gmail.com>
2024-06-20 13:01:29 -0400

    Fix try on building failure on github.

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,3 @@
-export { default as Breadcrumbs } from './breadcrumbs'
-export { default as Branding } from './branding'
-export { default as MainMenu } from './main-menu'
+export { default as Breadcrumbs } from '@/components/ui/breadcrumbs'
+export { default as Branding } from '@/components/ui/branding'
+export { default as MainMenu } from '@/components/ui/main-menu'
