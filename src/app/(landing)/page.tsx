c356a3968f6cb8c381990f74aa0df3f25146bdb6
Alex Alvarez Gárciga <aagarciga@gmail.com>
2024-07-06 23:31:24 -0400

    Changed Landing Page presentational text to be more personal.

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -17,10 +17,16 @@ export default function Home() {
         <header className={[].join(' ')} >
           <Brand size="large" />
         </header>
-        <p className={[].join(' ')}>I am Alex, a software engineer with over a decade of experience continuously honing my skills in crafting web applications, designing user-friendly interfaces, and studying algorithms & data structures.</p>
+        {/* <p className={[].join(' ')}>It is so nice to meet you! I am Alex, a software engineer with over a decade of experience continuously honing my skills in crafting web applications, designing user-friendly interfaces, and studying algorithms & data structures.</p>
         <p className={[].join(' ')}>My goal is to leverage my skills and knowledge to build innovative solutions that enhance people&apos;s lives and experiences.</p>
         <p className={[].join(' ')}>I enjoy mentoring, collaborating with cross-functional teams, and implementing agile methodologies to deliver user-friendly, maintainable, and high-performance software. I am always eager to learn and adopt new languages, frameworks, and tools to meet project needs and solve real-world problems.</p>
-        <p className={[].join(' ')}>In my past role as a lead front-end engineer at Origin IT Solutions, I combine my passion for web development and design with my interest in cloud infrastructure and open web technologies. I have more than 12 years of experience in creating and improving web applications for various clients and industries, using HTML, ES5, ES6, Typescript, React, CSS, Next.js, Figma, C# and .Net Technologies.</p>
+        <p className={[].join(' ')}>In my past role as a lead front-end engineer at Origin IT Solutions, I combine my passion for web development and design with my interest in cloud infrastructure and open web technologies. I have more than 12 years of experience in creating and improving web applications for various clients and industries, using HTML, ES5, ES6, Typescript, React, CSS, Next.js, Figma, C# and .Net Technologies.</p> */}
+
+
+        <p>It is so nice to meet you!, I am Alex. As a web developer with over a decade of experience, I have managed to stay on top of what users expect from modern applications.</p>
+        <p>I have chosen to master JavaScript, HTML5, and CSS3, specializing in front-end web development because I enjoy implementing and delivering high-quality UI/UX designs. While I am proud of my sensibilities regarding aesthetically pleasing yet functional design, I am not a designer by trade, and I have immense respect for those who are.</p>
+        <p>As a software engineer, I strive to understand how everything works and see all processes as a whole. This makes me a generalist, and I have often found myself spearheading internal tools, proof of concepts, or initiating greenfield projects in my past roles. However, in the past I have tried to avoid spreading myself too thin by focusing on C#, .NET technologies, and Azure as the workspace to become proficient at. So I can be a jack of all trades, master of one.</p>
+        {/* <p>Throughout my career, I have worked with Delphi, PHP, Visual FoxPro, Java, C/C++, Android, Node.js, Angular, and other technologies. Currently, I enjoy building applications with React, React Native, Next.js, and .NET Core. I am always eager to learn new languages or technologies when required. I can also switch between paradigms, from Object-Oriented Programming to functional approaches, comfortably writing imperative or declarative code as needed.</p> */}
         <Button className={[].join(' ')} label="Get my resume" primary Icon={DocumentTextIcon} onClick={() => router.push('/resume')} />
 
 
--- src/app/ui/experience.tsx
+++ src/app/ui/experience.tsx
@@ -22,7 +22,7 @@ export default function Experience({ className = '' }: ExperienceProps) {
         'Led the development of a healthcare and wellness solution for medical screening and doctor-patient shared decision-making.',
         'Implemented best practices to drive the software development lifecycle through Microsoft Azure DevOps and agile methodologies.',
         'Served as a consultant to clients on UI/UX design and front-end implementation.',
-        'Integrated new features and improved performance in existing products, enhancing client trust in the company&apos;s current solutions and services.',
+        'Integrated new features and improved performance in existing products, enhancing client trust in the company\'s current solutions and services.',
         'Architected, designed, and implemented a React Native mobile application for healthcare and health insurance using Microsoft Azure Identity Platform and zero-trust security.',
       ],
     },
@@ -31,7 +31,7 @@ export default function Experience({ className = '' }: ExperienceProps) {
       duration: '08/2016 - 03/2020',
       role: 'Chief Technology Officer',
       description: [
-        'Developed and implemented the company&apos;s overall technology strategy, roadmap, and vision to support business objectives and enhance operational efficiency.',
+        'Developed and implemented the company\'s overall technology strategy, roadmap, and vision to support business objectives and enhance operational efficiency.',
         'Directed the IT department, including staff recruitment, training, performance evaluation, budgeting, and resource allocation for technology investments.',
         'Worked closely with other departments to align technology initiatives with business requirements and support cross-functional projects.',
         'Led digital transformation initiatives, such as implementing new technologies, automation, and digital platforms to improve customer experience, streamline operations, and drive business growth.',
@@ -64,6 +64,7 @@ export default function Experience({ className = '' }: ExperienceProps) {
       <header>
         <h2>Experience</h2>
       </header>
+      <p className={styles.description}>Throughout my career, I have worked with Delphi, PHP, Visual FoxPro, Java, C/C++, Android, Node.js, Angular, and other technologies. Currently, I enjoy building applications with React, React Native, Next.js, and .NET Core. I am always eager to learn new languages or technologies when required. I can also switch between paradigms, from Object-Oriented Programming to functional approaches, comfortably writing imperative or declarative code as needed.</p>
       {experienceItems.map((item, index) => (
         <Section
           key={index}
